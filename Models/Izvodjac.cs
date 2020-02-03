ca65959735d9df53d4860e53487214a0e18b69e9
Trbuhozborac <nemanjapipi@hotmail.com>
2020-02-03 22:06:46 +0100

    All finished exept Izvodjaci page

---
+++ Models/Izvodjac.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrogMusicFest.Models
+{
+    public class Izvodjac
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string VrstaMuzike { get; set; }
+        public DateTime DanNastupa { get; set; }
+        public DateTime? VremeNastupa { get; set; }
+
+        //public string Image { get; set; }
+    }
+}
